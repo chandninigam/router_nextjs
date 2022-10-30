b6ea0b49a6aee4a09fb057a620798ed5672745ef
chandninigam <51984693+chandninigam@users.noreply.github.com>
2022-10-30 13:47:56 +0530

    Added basic Form

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,25 @@
 import Link from "next/link";
+import { useState } from "react";
 
 export default function Index() {
+  const [value, setValue] = useState("");
   return (
     <div>
+      {/* Login Page */}
+      <h1>Login</h1>
+      <form>
+        <input
+          type="text"
+          placeholder="User Name"
+          onChange={(e) => {
+            setValue(e.target.value);
+          }}
+        />
+        <input type="number" placeholder="Password" />
+      </form>
+      <Link href="/contact">submit</Link>
       <h1>Index Route</h1>
       <Link href="/about">Go to About</Link>
-      <br />
-      <Link href="/contact">Go to Contact Us</Link>
-      <br />
       <Link href="/blogs">GO to blogs</Link>
     </div>
   );
